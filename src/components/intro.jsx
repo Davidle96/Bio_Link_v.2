65f732debef7cd9e78a4aed5defbdb2569a709ca
Davidle96 <tranquocdatnt1990@gmail.com>
2025-11-05 02:36:48 +0700

    update lại 1 tý

---
+++ src/components/intro.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Intro() {
+    return (
+        <div className="intro">
+            <h1>Hi, I'm <span className="name">Sarvesh</span></h1>
+            <p>Frontend Developer</p>
+        </div>
+    );
+}
