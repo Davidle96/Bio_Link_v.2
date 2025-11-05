80ee54a9473ba5725876e1a5d69c1162c6d3de95
Davidle96 <tranquocdatnt1990@gmail.com>
2025-11-05 17:29:31 +0700

    update intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css'
+import Intro from './components/intro';
+import Background from './components/Background';
 
-function App() {
+export default function App() {
 
   return (
     <>
+      <Intro />
+      <Background
+        glitchSpeed={10}
+        centerVignette={true}
+        outerVignette={true}
+        smooth={true}
+      />
+      <div className="min-h-screen flex items-center justify-center bg-none text-white text-4xl font-bold" style={{ zIndex: "1" }}>
+        Tailwind is working!
+      </div>
     </>
   )
 }
-
-export default App
